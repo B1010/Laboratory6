2e3f8123f6d0add5a38ba3514930160dbc2f8990
Артём Акопьян <artemik-98@outlook.com>
2016-12-12 23:04:04 +0400

    Добавлен номер 12

---
+++ Exersice12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exersice12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateUSA = "4/23/2011";
+            char[] separators = { '/' };
+
+            string[] dateUSA_arr = dateUSA.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("Новый формат даты: {0}.{1}.{2}", dateUSA_arr[1], dateUSA_arr[0], dateUSA_arr[2]);
+        }
+    }
+}
