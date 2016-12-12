5468b85cd2546cffa92a661db782f95a9f5c1922
Артём Акопьян <artemik-98@outlook.com>
2016-12-12 21:55:25 +0400

    Добавлен номер 9

---
+++ Exersice9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exersice9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите строку: ");
+            string textFind = Console.ReadLine();
+            Console.Write("Введите подстроку: ");
+            string Find = Console.ReadLine();
+            int index = 0;
+
+            if (textFind.IndexOf(Find) > -1)
+            {
+                index = textFind.IndexOf(Find) + 1;
+                Console.WriteLine("Найдена подстрока, индекс первой буквы: " + index);
+            }
+            else
+            {
+                Console.WriteLine("Подстрока не найдена");
+            }
+        }
+    }
+}
