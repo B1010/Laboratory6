ff29cf3c1185160c44afc1e1e1d9d3dad639f592
Артём Акопьян <artemik-98@outlook.com>
2016-12-27 01:21:47 +0400

    Готооооов номер 3

--- Exersice3/Program.cs
+++ Exersice3/Program.cs
@@ -7,46 +7,28 @@ namespace Exersice3
         static void Main()
         {
             bool result = false;
-            int sovpad = 0;
-            char[] separators = { ',', ' ', ';' };
+            int sovpad = 0, g = 0, x = -1;
+            string[] family = new string[3];
+            
+            string search = " ИВАН ";
 
             Console.WriteLine("Ввседите 3 разных ФИО:");
-            string ifam = Console.ReadLine().ToUpper();
-            string jfam = Console.ReadLine().ToUpper();
-            string lfam = Console.ReadLine().ToUpper();
-            string search = "ИВАН";
+            for(int i=0; i<3; i++)
+            {
+                family[i] = Console.ReadLine().ToUpper();
+            }
 
-            string[] ifam_words = ifam.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-            string[] jfam_words = jfam.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-            string[] lfam_words = lfam.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in family)
+            {
+                while (g != -1)
+                {
+                    g = word.IndexOf(search, x + 1);
+                    x = g;
+                    sovpad++;
+                    result = true;
+                }
+            }
 
-            foreach (string word in ifam_words)
-            {
-                if (search.Equals(word))
-                {
-                    result = true;
-                    sovpad++;
-                    break;
-                }
-            }
-            foreach (string word in jfam_words)
-            {
-                if (search.Equals(word))
-                {
-                    result = true;
-                    sovpad++;
-                    break;
-                }
-            }
-            foreach (string word in lfam_words)
-            {
-                if (search.Equals(word))
-                {
-                    result = true;
-                    sovpad++;
-                    break;
-                }
-            }
             if (result)
             {
                 Console.WriteLine("\nНайдено совпадение! Количество человек с именем Иван: {0}", sovpad);
